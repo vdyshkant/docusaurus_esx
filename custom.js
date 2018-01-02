56ee48b3503e850ef326256dd80631b07a844734
Vadim Dyshkant <develop@fastmail.co.uk>
2018-01-02 14:29:45 +0200

    made changes for custom slidetoggle script

--- custom.js
+++ custom.js
@@ -3,6 +3,6 @@ $( document ).ready(function() {
     // console.log( "ready!" );
     $(".sp-js .sp__title").click (function(e){
         e.preventDefault();
-        $(this).find('.sp__content').slideToggle();
+        $(this).next().slideToggle();
     });
 });
